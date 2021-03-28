872db82ae832f729e6e379abceb3ef7bf60829ef
Vinicius Martins <vinicius.silva@matilha.digital>
2021-03-28 15:49:45 -0300

    Child components

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>UserView</div>
+  <div>
+    <VUserSearchForm />
+    <VUserProfile />
+  </div>
 </template>
 
 <script>
+import VUserSearchForm from "@/components/VUserSearchForm";
+import VUserProfile from "@/components/VUserProfile";
+
 export default {
   name: "UserView",
+
+  components: {
+    VUserSearchForm,
+    VUserProfile,
+  },
 };
 </script>
--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,12 +1,33 @@
 import { shallowMount } from "@vue/test-utils";
 import UserView from "@/views/UserView";
+import VUserSearchForm from "@/components/VUserSearchForm";
+import VUserProfile from "@/components/VUserProfile";
 
 describe("UserView", () => {
-  it("renders the component", () => {
-    // arrange
+  const build = () => {
     const wrapper = shallowMount(UserView);
 
+    return {
+      wrapper,
+      userSearchForm: () => wrapper.findComponent(VUserSearchForm),
+      userProfile: () => wrapper.findComponent(VUserProfile),
+    };
+  };
+
+  it("renders the component", () => {
+    // arrange
+    const { wrapper } = build();
     // assert
+
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("renders main child components", () => {
+    // arrange
+    const { userSearchForm, userProfile } = build();
+
+    // assert
+    expect(userSearchForm().exists()).toBe(true);
+    expect(userProfile().exists()).toBe(true);
   });
 });
